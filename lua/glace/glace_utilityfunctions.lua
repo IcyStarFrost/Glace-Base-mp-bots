662c149231ed7d830b201e69dea164b3c88e3c67
IcyStarFrost <frostlegion10@gmail.com>
2022-08-30 20:10:51 -0500

    Added a console command to automatically edit the navmesh

--- lua/glace/glace_utilityfunctions.lua
+++ lua/glace/glace_utilityfunctions.lua
@@ -11,6 +11,25 @@ if SERVER then
 
     end
 
+    function GlaceBase_AutoEditNavmesh()
+        local areas = navmesh.GetAllNavAreas()
+        for k,v in ipairs(areas) do
+            local adjareas = v:GetAdjacentAreas()
+            for i,l in ipairs(adjareas) do
+                if v:ComputeAdjacentConnectionHeightChange(l) > 20 then 
+                    v:Disconnect(l)
+                end
+                if v:ComputeAdjacentConnectionHeightChange(l) < -150 then 
+                    v:Disconnect(l)
+                end
+    
+            end
+        end
+        print("Navmesh has been edited to help with pathfinding!")
+
+        navmesh.Save()
+    end
+
 
     concommand.Add("glacebase_kickbots", GlaceBase_KickAllBots)
 
